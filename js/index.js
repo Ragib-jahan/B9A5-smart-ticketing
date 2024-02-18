8f5c117da97d23f692d733104744757e6bda2166
Ragib-jahan <ragibjahan01@gmail.com>
2024-02-19 01:53:20 +0600

    Added the suuccess section

--- js/index.js
+++ js/index.js
@@ -1,32 +1,59 @@
 function seatChoiceA1() {
     removeHiddenTicket('A1');
-    addBgInButton('buA1')
+    addBgInButton('buA1');
+    addTicketNumber('seat-add');
+    countSeatsLeft('seats-left');
 }
 function seatChoiceA2() {
     removeHiddenTicket('A2');
     addBgInButton('buA2')
+    addTicketNumber('seat-add');
+    countSeatsLeft('seats-left');
 }
 function seatChoiceA3() {
     removeHiddenTicket('A3');
-    addBgInButton('buA3')
+    addBgInButton('buA3');
+    addTicketNumber('seat-add');
+    countSeatsLeft('seats-left');
 }
 function seatChoiceA4() {
     removeHiddenTicket('A4');
-    addBgInButton('buA4')
+    addBgInButton('buA4');
+    addTicketNumber('seat-add');
+    countSeatsLeft('seats-left');
 }
 function seatChoiceB1() {
     removeHiddenTicket('B1');
-    addBgInButton('buB1')
+    addBgInButton('buB1');
+    addTicketNumber('seat-add');
+    countSeatsLeft('seats-left');
 }
 function seatChoiceB2() {
     removeHiddenTicket('B2');
-    addBgInButton('buB2')
+    addBgInButton('buB2');
+    addTicketNumber('seat-add');
+    countSeatsLeft('seats-left');
 }
 function seatChoiceB3() {
     removeHiddenTicket('B3');
-    addBgInButton('buB3')
+    addBgInButton('buB3');
+    addTicketNumber('seat-add');
+    countSeatsLeft('seats-left');
 }
 function seatChoiceB4() {
     removeHiddenTicket('B4');
-    addBgInButton('buB4')
+    addBgInButton('buB4');
+    addTicketNumber('seat-add');
+    countSeatsLeft('seats-left');
 }
+
+function successSectionOn(){
+ removeHiddenTicket('success-section');
+ addHiddenTicket('all-section');
+
+}
+
+
+function goTicketSection() {
+    window.scrollBy(0, window.innerWidth);
+  }
--- js/utilije.js
+++ js/utilije.js
@@ -1,9 +1,33 @@
-function removeHiddenTicket(elementId){
+function removeHiddenTicket(elementId) {
     const element = document.getElementById(elementId);
     element.classList.remove('hidden');
 }
-function addBgInButton(elementId){
+function addHiddenTicket(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.add('hidden');
+}
+function addBgInButton(elementId) {
     const element = document.getElementById(elementId);
     element.classList.add('bg-[#1DD100]', 'text-white');
 }
+
+function addTicketNumber(elementId) {
+    const element = document.getElementById(elementId);
+    const elementText = element.innerText;
+    const currentValue = parseInt(elementText);
+    const newValue = currentValue + 1;
+    element.innerText = newValue;
+}
+
+function countSeatsLeft(elementId) {
+    const element = document.getElementById(elementId);
+    const elementText = element.innerText;
+    const currentValue = parseInt(elementText);
+    const newValue = currentValue - 1;
+    element.innerText = newValue;
+    if (newValue === -1) {
+        element.innerText = newValue + 1;
+    }
+}
+
 
